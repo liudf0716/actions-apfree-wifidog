d5cbfded380d680d983ff6170cd7d438c73705ad
Dengfeng Liu <liudf0716@gmail.com>
2022-01-23 11:31:20 +0800

    Update apfreewifidog.sh

--- apfreewifidog.sh
+++ apfreewifidog.sh
@@ -2,4 +2,4 @@
 
 # change apfree-wifidog version to master
 
-sed -i 's/=4.08.1771/=master/g'  package/feeds/packages/apfree-wifidog/Makefile
+sed -i 's/=3.11.1716/=master/g'  package/apfree-wifidog/Makefile
